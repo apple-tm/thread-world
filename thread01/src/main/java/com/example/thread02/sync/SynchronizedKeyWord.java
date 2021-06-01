73b0d230f61d4846d6e9caee536ad31b0f82b324
‘wul’ <279359180@qq.com>
2021-06-01 15:18:10 +0800

    #014 多线程学习

--- thread01/src/main/java/com/example/thread02/sync/SynchronizedKeyWord.java
+++ thread01/src/main/java/com/example/thread02/sync/SynchronizedKeyWord.java
@@ -7,9 +7,15 @@ package com.example.thread02.sync;
  */
 public class SynchronizedKeyWord {
 
-    private static int money = 0;
+    public static   int money = 0;
 
-    public  void test1() {
+    /**
+     * ++
+     * 1. 获取值
+     * 2. 值+1
+     * 3. 设置值
+     */
+    public static synchronized   void test1() {
         money++;
     }
 
@@ -18,12 +24,30 @@ public class SynchronizedKeyWord {
     }
 
     public static void main(String[] args) {
+        long start = System.currentTimeMillis();
         SynchronizedKeyWord synchronizedKeyWord = new SynchronizedKeyWord();
-        synchronizedKeyWord.test1();
-        new Thread(() -> {
-            synchronizedKeyWord.test1();
-            System.out.println(money);
-        }).start();
-        System.out.println(money);
+        SynchronizedKeyWord synchronizedKeyWord2 = new SynchronizedKeyWord();
+
+
+        for (int i=1; i<=50; i++) {
+            new Thread(() -> {
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                SynchronizedKeyWord.test1();
+                SynchronizedKeyWord.test1();
+            }).start();
+        }
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(SynchronizedKeyWord.money);
+        System.out.println(Runtime.getRuntime().maxMemory());
     }
 }
